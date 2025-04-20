501e2067b6aed7913b0a361ff1184d2934fee435
Barend Scholtus <barend.scholtus@gmail.com>
2025-04-20 16:26:20 +0700

    Base keymap based on Redox. Luna animation.

---
+++ keyboards/neodox/keymaps/default/rules.mk
@@ -0,0 +1,11 @@
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+ENCODER_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
+COMBO_ENABLE = yes
+WPM_ENABLE = yes
+
+ifeq ($(strip $(OLED_ENABLE)), yes)
+	SRC += oled.c
+	SRC += luna.c
+endif
